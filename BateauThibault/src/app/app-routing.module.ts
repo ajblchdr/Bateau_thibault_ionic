4c2c85343f2fa81d17cfc3b33ab2d01d564fc3c2
karkina <nico.soumare@gmail.com>
2021-10-19 15:13:17 +0200

    Home react

--- BateauThibault/src/app/app-routing.module.ts
+++ BateauThibault/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'accueil',
+    loadChildren: () => import('./accueil/accueil.module').then( m => m.AccueilPageModule)
   },
+
 ];
 
 @NgModule({
